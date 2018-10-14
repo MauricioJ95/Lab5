dd7b7953d236c4d5b2af4cbe90b669e4f1a41cb5
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-14 17:44:50 -0400

    intial commit

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -10,6 +10,28 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
+
+            string input;
+            do
+            {
+                Console.WriteLine("Please input a number between 1-10");
+                int num = int.Parse(Console.ReadLine());
+
+                long factorial = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    factorial *= i;
+                }
+
+                Console.WriteLine("The factorial of {0} is {1}", num, factorial);
+                Console.ReadKey();
+
+
+                Console.WriteLine("Continue? Y/N)");
+                input = Console.ReadLine();
+
+            }
+            while (input == "y");
         }
     }
 }
